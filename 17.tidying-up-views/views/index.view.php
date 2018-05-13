11a9eacd18f97f556de52dcb928d245731ab68f4
FrederickFoxxx <fwhduffield@gmail.com>
2018-05-13 20:46:15 +0100

    Step 17: DRY up your viewa

---
+++ 17.tidying-up-views/views/index.view.php
@@ -0,0 +1,16 @@
+<?php require 'partials/head.php';?>
+
+<h1>Task</h1>
+      <ul>
+        <?php foreach ($tasks as $task): ?>
+            <li>
+                <?php if ($task->completed): ?>
+                    <strike><?=$task->description;?></strike>
+                <?php else: ?>
+                    <?=$task->description;?>
+                <?php endif;?>
+            </li>
+        <?php endforeach;?>
+    </ul>
+
+<?php require 'partials/footer.php';?>
